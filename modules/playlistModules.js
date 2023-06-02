3102a922b5e1e1d01022a9036bb2c67df6f1efa6
AnthonySinitsa <itonyemail@gmail.com>
2023-06-02 03:07:20 +0000

    dontoutch

--- modules/playlistModules.js
+++ modules/playlistModules.js
@@ -7,24 +7,24 @@ const verifyUser = require('./auth');
 
 // This exports multiple functions at once.
 playlistModule.getPlaylist = async (req, res, next) => {
-  /* verifyUser(req, async (err, user) => {
-    if (err) {
-      console.error(err);
-      res.send('invalid token');
-    } else { */
-      let params = {};
-      if (req.query.email) {
-        params.email = req.query.email
-      }
-      try {
-        // This talks to your database
-        // This is referencing your model
-        let results = await PlaylistSuperModel.find(params);
-        res.status(200).send(results);
-      } catch (err) {
-        next(err)
-      }
-    // }
+  // verifyUser(req, async (err, user) => {
+  //   if (err) {
+  //     console.error(err);
+  //     res.send('invalid token');
+  //   } else {
+  let params = {};
+  if (req.query.email) {
+    params.email = req.query.email
+  }
+  try {
+    // This talks to your database
+    // This is referencing your model
+    let results = await PlaylistSuperModel.findOne(params);
+    res.status(200).send(results);
+  } catch (err) {
+    next(err)
+  }
+  //   }
   // });
 };
 
@@ -34,13 +34,13 @@ playlistModule.postPlaylist = async (req, res, next) => {
   //     console.error(err);
   //     res.send('invalid token');
   //   } else {
-    console.log('test', req.body);
-      try {
-        let createdPlaylist = await PlaylistSuperModel.create(req.body);
-        res.status(200).send(createdPlaylist);
-      } catch (err) {
-        next(err);
-      }
+  // console.log('test', req.body);
+  try {
+    let createdPlaylist = await PlaylistSuperModel.create(req.body);
+    res.status(200).send(createdPlaylist);
+  } catch (err) {
+    next(err);
+  }
   //   }
   // });
 };
@@ -67,7 +67,13 @@ playlistModule.deletePlaylist = async (req, res, next) => {
 
 playlistModule.putPlaylist = async (req, res, next) => {
   try {
-    await PlaylistSuperModel.findOneAndUpdate({ email: req.body.email }, {$set: { songs: req.body.songs } });
+    let id = req.params.id;
+    let playlistFromReq = req.body;
+    let options = {
+      new: true,
+      overwrite: true
+    };
+    await PlaylistSuperModel.findByIdAndUpdate(id, playlistFromReq, options);
     res.status(200).send('Playlist Updated');
   } catch (err) {
     next(err);
