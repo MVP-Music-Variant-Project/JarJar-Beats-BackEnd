b9314c38e4753fec29d031252ebf88647b981597
AnthonySinitsa <itonyemail@gmail.com>
2023-06-02 17:14:00 +0000

    fiex auth

--- modules/playlistModules.js
+++ modules/playlistModules.js
@@ -7,50 +7,50 @@ const verifyUser = require('./auth');
 
 // This exports multiple functions at once.
 playlistModule.getPlaylist = async (req, res, next) => {
-  // verifyUser(req, async (err, user) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.send('invalid token');
-  //   } else {
-  let params = {};
-  if (req.query.email) {
-    params.email = req.query.email
-  }
-  try {
-    // This talks to your database
-    // This is referencing your model
-    let results = await PlaylistSuperModel.findOne(params);
-    res.status(200).send(results);
-  } catch (err) {
-    next(err)
-  }
-  //   }
-  // });
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send('invalid token');
+    } else {
+      let params = {};
+      if (req.query.email) {
+        params.email = req.query.email
+      }
+      try {
+        // This talks to your database
+        // This is referencing your model
+        let results = await PlaylistSuperModel.findOne(params);
+        res.status(200).send(results);
+      } catch (err) {
+        next(err)
+      }
+    }
+  });
 };
 
 playlistModule.postPlaylist = async (req, res, next) => {
-  // verifyUser(req, async (err, user) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.send('invalid token');
-  //   } else {
-  // console.log('test', req.body);
-  try {
-    let createdPlaylist = await PlaylistSuperModel.create(req.body);
-    res.status(200).send(createdPlaylist);
-  } catch (err) {
-    next(err);
-  }
-  //   }
-  // });
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send('invalid token');
+    } else {
+      console.log('test', req.body);
+      try {
+        let createdPlaylist = await PlaylistSuperModel.create(req.body);
+        res.status(200).send(createdPlaylist);
+      } catch (err) {
+        next(err);
+      }
+    }
+  });
 };
 
 playlistModule.deletePlaylist = async (req, res, next) => {
-  //  verifyUser(req, async (err, user) => {
-  //   if (err) {
-  //     console.error(err);
-  //     res.send('invalid token');
-  //   } else { 
+  verifyUser(req, async (err, user) => {
+    if (err) {
+      console.error(err);
+      res.send('invalid token');
+    } else {
       try {
         let id = req.params.id;
         await PlaylistSuperModel.findByIdAndDelete(id);
@@ -58,8 +58,8 @@ playlistModule.deletePlaylist = async (req, res, next) => {
       } catch (err) {
         next(err);
       }
-  //   }
-  // });
+    }
+  });
 }
 
 playlistModule.putPlaylist = async (req, res, next) => {
